3241ab3000a26e0b5aad37be652f30bdfa332ff3
RhancisDevs <129867977+RhancisDevs@users.noreply.github.com>
2024-07-18 17:05:40 +0800

    Update bloxfruit.lua

--- bloxfruit.lua
+++ bloxfruit.lua
@@ -1,19 +1,53 @@
-local player = game.Players.LocalPlayer
-local playerName = player.DisplayName
+local plr = game:GetService("Players").LocalPlayer
+local Notification = require(game:GetService("ReplicatedStorage").Notification)
+local Data = plr:WaitForChild("Data")
+local EXPFunction = require(game.ReplicatedStorage:WaitForChild("EXPFunction"))
+local LevelUp = require(game:GetService("ReplicatedStorage").Effect.Container.LevelUp)
+local Sound = require(game:GetService("ReplicatedStorage").Util.Sound)
+local LevelUpSound = game:GetService("ReplicatedStorage").Util.Sound.Storage.Other:FindFirstChild("LevelUp_Proxy") or game:GetService("ReplicatedStorage").Util.Sound.Storage.Other:FindFirstChild("LevelUp")
 
-local function sendNotification(title, text, duration)
-    game.StarterGui:SetCore("SendNotification", {
-        Title = title;
-        Text = text;
-        Duration = duration;
-    })
+function v129(p15)
+    local v130 = p15
+    while true do
+        local v131, v132 = string.gsub(v130, "^(-?%d+)(%d%d%d)", "%1,%2")
+        v130 = v131
+        if v132 == 0 then
+            break
+        end
+    end
+    return v130
 end
 
-sendNotification("Warning", playerName .. ", your actions are being monitored...", 3)
+Notification.new("<Color=Yellow>QUEST COMPLETED!<Color=/>"):Display()
+Notification.new("Earned <Color=Yellow>1,000,000,000,000 Exp.<Color=/> (+ None)"):Display()
+Notification.new("Earned <Color=Green>$25,000<Color=/>"):Display()
+plr.Data.Exp.Value = 999999999999
+plr.Data.Beli.Value = plr.Data.Beli.Value + 25000
+
+local delay = 0
+local count = 0
+while plr.Data.Level.Value < 10000 and plr.Data.Exp.Value - EXPFunction(Data.Level.Value) > 0 do
+    plr.Data.Exp.Value = plr.Data.Exp.Value - EXPFunction(Data.Level.Value)
+    plr.Data.Level.Value = plr.Data.Level.Value + 1
+    plr.Data.Points.Value = plr.Data.Points.Value + 3
+    LevelUp({ plr })
+    Sound.Play(Sound, LevelUpSound.Value)
+    Notification.new("<Color=Green>LEVEL UP!<Color=/> (" .. plr.Data.Level.Value .. ")"):Display()
+    count = count + 1
+    if count >= 5 then
+        delay = tick()
+        count = 0
+        wait(2)
+    end
+end
+
+local playerName = plr.DisplayName
+
+Notification.new("<Color=Yellow>Warning</Color>: " .. playerName .. ", your actions are being monitored..."):Display()
 wait(3)
-sendNotification("Notice", playerName .. ", exploiting is against the rules and ruins the game for others.", 5)
+Notification.new("<Color=Yellow>Notice</Color>: " .. playerName .. ", exploiting is against the rules and ruins the game for others."):Display()
 wait(5)
-sendNotification("Consequence", playerName .. ", you will be kicked from the game as a warning.", 2)
+Notification.new("<Color=Yellow>Consequence</Color>: " .. playerName .. ", you will be kicked from the game as a warning."):Display()
 wait(2)
 
-player:Kick("You were kicked from this experience: Exploiting is not allowed, " .. playerName .. ". Please play fair.")
+plr:Kick("You were kicked from this experience: You're permanently banned from this game.")
