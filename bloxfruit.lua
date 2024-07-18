cec73ec4165c8b30f06c53b00d57e9aef1c53d78
RhancisDevs <129867977+RhancisDevs@users.noreply.github.com>
2024-07-18 10:16:13 +0800

    Update bloxfruit.lua

--- bloxfruit.lua
+++ bloxfruit.lua
@@ -1,1 +1,11 @@
+local function sendNotification(title, text, duration)
+    game.StarterGui:SetCore("SendNotification", {
+        Title = title;
+        Text = text;
+        Duration = duration;
+    })
+end
+
+sendNotification("Loading", "The game is loading, please wait...", 2)
+wait(2)
 game.Players.LocalPlayer:Kick("You were kicked from this experience: You're permanently banned from this game. (Error Code: 267)")
