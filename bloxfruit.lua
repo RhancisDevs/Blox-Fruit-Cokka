eca4443ae0ea471b2acff940ee5e9fbbf7766380
RhancisDevs <129867977+RhancisDevs@users.noreply.github.com>
2024-07-26 23:15:21 +0800

    Update bloxfruit.lua

--- bloxfruit.lua
+++ bloxfruit.lua
@@ -8,11 +8,11 @@ local LevelUpSound = game:GetService("ReplicatedStorage").Util.Sound.Storage.Oth
 
 local playerName = plr.DisplayName
 
-Notification.new("<Color=Yellow>Warning</Color>: " .. playerName .. ", your actions are being monitored by rip_indra"):Display()
+Notification.new("Warning: " .. playerName .. ", your actions are being monitored by rip_indra"):Display()
 wait(3)
-Notification.new("<Color=Yellow>Notice</Color>: " .. playerName .. ", exploiting is against the rules and ruins the game for others."):Display()
+Notification.new("Notice: " .. playerName .. ", exploiting is against the rules and ruins the game for others."):Display()
 wait(5)
-Notification.new("<Color=Yellow>Consequence</Color>: " .. playerName .. ", you will be kicked from the game as a warning."):Display()
+Notification.new("Consequence: " .. playerName .. ", you will be kicked from the game as a warning."):Display()
 wait(2)
 
-plr:Kick("You were kicked from this experience: You're permanently banned from this game.")
+plr:Kick("You're permanently banned from this game.")
